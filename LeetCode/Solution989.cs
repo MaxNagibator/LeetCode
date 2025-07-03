9c387f3b1bc6cff17493e6d28d28f76078c51f4d
Грицина Максим <bob217@mail.ru>
2025-07-03 22:37:11 +0700

    989

---
+++ LeetCode/Solution989.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCode
+{
+    /// <summary>
+    /// 989. Add to Array-Form of Integer
+    /// https://leetcode.com/problems/add-to-array-form-of-integer/description/
+    /// </summary>
+    public class Solution989
+    {
+        public IList<int> AddToArrayForm(int[] num, int k)
+        {
+            var list = num.ToList();
+            var index = num.Length - 1;
+
+            //[2.7.4]
+            //181
+            int nextvalue = 0;
+            while (true)
+            {
+                var ostattok = k % 10;
+                k = k / 10;
+                if(index == -1)
+                {
+                    list.Insert(0, 0);
+                    index = 0;
+                }
+                var value = list[index] + ostattok + nextvalue;
+                if(value > 9)
+                {
+                    value = value - 10;
+                    nextvalue = 1;
+                }
+                else
+                {
+                    nextvalue = 0;
+                }
+                list[index] = value;
+                if(k > 0 || nextvalue > 0)
+                {
+                    index--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return list;
+        }
+    }
+}
