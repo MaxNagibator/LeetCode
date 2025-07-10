5ad9e2f3302ad5bdb937a63411f615ee33c15bd8
Грицина Максим <bob217@mail.ru>
2025-07-10 23:04:52 +0700

    200+859

---
+++ LeetCode/Solution200.cs
@@ -0,0 +1,114 @@
+﻿
+namespace LeetCode
+{
+    /// <summary>
+    /// 200. Number of Islands
+    /// https://leetcode.com/problems/number-of-islands/description/
+    /// </summary>
+    public class Solution200
+    {
+        public int NumIslands(char[][] grid)
+        {
+            var check = new bool[grid.Length, grid[0].Length];
+            var count = 0;
+            var stack = new Stack<Tuple<int, int>>();
+            for (var i = 0; i < check.GetLength(0); i++)
+            {
+                for (var j = 0; j < check.GetLength(1); j++)
+                {
+                    if (check[i, j])
+                    {
+                        continue;
+                    }
+
+                    if (grid[i][j] == '1')
+                    {
+                        count++;
+
+                        var i1 = i;
+                        var j1 = j;
+                        while (true)
+                        {
+                            //up
+                            var i1next = i1 - 1;
+                            var j1next = j1;
+                            var result = Move();
+                            if (result)
+                            {
+                                continue;
+                            }
+
+                            //right
+
+                            i1next = i1;
+                            j1next = j1 + 1;
+                            result = Move();
+                            if (result)
+                            {
+                                continue;
+                            }
+
+                            //left
+
+                            i1next = i1;
+                            j1next = j1 - 1;
+                            result = Move();
+                            if (result)
+                            {
+                                continue;
+                            }
+
+
+                            //down
+                            i1next = i1 + 1;
+                            j1next = j1;
+                            result = Move();
+                            if (result)
+                            {
+                                continue;
+                            }
+
+                            if (stack.Count > 0)
+                            {
+                                var prevPosition = stack.Pop();
+                                i1 = prevPosition.Item1;
+                                j1 = prevPosition.Item2;
+                            }
+                            else
+                            {
+                                break;
+                            }
+
+                            bool Move()
+                            {
+                                if (i1next < 0 || i1next >= check.GetLength(0))
+                                {
+                                    return false;
+                                }
+                                if (j1next < 0 || j1next >= check.GetLength(1))
+                                {
+                                    return false;
+                                }
+                                if (check[i1next, j1next] == false)
+                                {
+                                    check[i1next, j1next] = true;
+                                    if (grid[i1next][j1next] == '1')
+                                    {
+                                        stack.Push(new Tuple<int, int>(i1, j1));
+                                        i1 = i1next;
+                                        j1 = j1next;
+                                        return true;
+                                    }
+                                }
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            return count;
+        }
+    }
+}
---
+++ LeetCode/Solution859.cs
@@ -0,0 +1,68 @@
+﻿namespace LeetCode
+{
+    /// <summary>
+    /// 859. Buddy Strings
+    /// https://leetcode.com/problems/buddy-strings/description/
+    /// </summary>
+    public class Solution859
+    {
+        public bool BuddyStrings(string s, string goal)
+        {
+            if (s.Length != goal.Length)
+            {
+                return false;
+            }
+
+            byte notEqualsCount = 0;
+            int? char1Index = null;
+            int? char2Index = null;
+            for (var i = 0; i < goal.Length; i++)
+            {
+                if (goal[i] != s[i])
+                {
+                    if (char1Index != null)
+                    {
+                        char2Index = i;
+                    }
+                    else
+                    {
+                        char1Index = i;
+                    }
+                    notEqualsCount++;
+                    if (notEqualsCount > 2)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            if (notEqualsCount == 0)
+            {
+                // aba    aba  -> aab
+                var sortArray = s.ToCharArray().OrderBy(x => x).ToArray();
+                for (var i = 0; i < sortArray.Length - 1; i++)
+                {
+                    if (sortArray[i] == sortArray[i + 1])
+                    {
+                        return true;
+                    }
+                }
+                // строки полностью совпадают
+                return false;
+            }
+
+            if (notEqualsCount == 1)
+            {
+                return false;
+            }
+
+            // ab   ba    0  1
+            if (goal[char1Index!.Value] == s[char2Index!.Value] &&
+                goal[char2Index.Value] == s[char1Index.Value])
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
